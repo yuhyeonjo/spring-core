ace908b7430fe1639667d195348aa1ec8a71bd08
yuhyeonjo <jo.yuhyeon16@gmail.com>
2022-07-06 16:50:10 +0900

    생성자 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,13 +9,20 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    // 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드 고쳐야 함 (새로운 할인 정책 적용의 문제점)
+    private final MemberRepository memberRepository;
+    // 1. 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드 고쳐야 함 (새로운 할인 정책 적용의 문제점)
     // DIP 위반 ( DiscountPolicy 가 아닌 구체 클래스에 의존하고 있음)
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
-    // RateDiscountPolicy 로 바꾸는 순간 OCP 위반
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 2. RateDiscountPolicy 로 바꾸는 순간 OCP 위반
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
